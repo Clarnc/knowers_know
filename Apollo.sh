98ada41b09e8061d592885e6228fe9587abd06e1
Clarnc <claus909808@gmail.com>
2025-11-26 13:48:47 +0100

    Apollo fix

--- Apollo.sh
+++ Apollo.sh
@@ -10,7 +10,6 @@ fi
 START="ThemedSquadOverlay.lua: Mission name: Apollo (Lua)"
 END="Net \[Info\]: Replication count by type:"
 
-# Efficient bottom-up read
 log_segment=$(tac "$LOGFILE" | awk -v start="$END" -v end="$START" '
   index($0, start) {found_start=1}
   found_start {block = $0 "\n" block}
@@ -19,7 +18,7 @@ log_segment=$(tac "$LOGFILE" | awk -v start="$END" -v end="$START" '
 
 [[ -z "$log_segment" ]] && { echo "Error: Could not find replication block."; exit 1; }
 
-declare -A sound_paths=(
+declare -A tile_paths=(
   ["/Lotus/Sounds/Ambience/OrokinMoon/MoonIntHallsOfJudgement/"]="HallsOfJudgement"
   ["/Lotus/Sounds/Ambience/OrokinMoon/MoonIntCloister/"]="Cloister"
   ["/Lotus/Sounds/Ambience/OrokinMoon/MoonIntEndurance/"]="Endurance"
@@ -28,12 +27,20 @@ declare -A sound_paths=(
 )
 
 matches=()
-for path in "${!sound_paths[@]}"; do
-  grep -Fq "$path" <<< "$log_segment" && matches+=("${sound_paths[$path]}")
+for path in "${!tile_paths[@]}"; do
+  grep -Fq "$path" <<< "$log_segment" && matches+=("${tile_paths[$path]}")
 done
 
-if [ "${#matches[@]}" -ge 2 ]; then
-  echo "${matches[@]}"
+allowed=("power" "HallsOfJudgement" "Endurance" )
+
+filtered=()
+for match in "${matches[@]}"; do
+  [[ " ${allowed[*]} " =~ " ${match} " ]] && filtered+=("$match")
+done
+
+if [ "${#filtered[@]}" -ge 2 ]; then
+  echo "${filtered[@]}"
 else
   echo "Bad tile. Skip"
 fi
+
