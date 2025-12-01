73082c1a588f39214176e299ad5b9cf3881c2aa7
Clarnc <clarn@clarncc.cc>
2025-12-01 19:23:51 +0100

    syntax error fix

--- start.sh
+++ start.sh
@@ -183,7 +183,7 @@ while true; do
       ur) TITLE="Ur"; ICON_PATH="Icons/Ur_icon.png";;
       olympus) TITLE="Olympus"; ICON_PATH="Icons/Olympus_icon.png";;
       tamu) TITLE="Tamu"; ICON_PATH="Icons/Tamu_icon.png";;
-      ganymde) TITLE="Ganymede"; ICON_PATH="Icons/Ganymede_icon.png";;
+      ganymede) TITLE="Ganymede"; ICON_PATH="Icons/Ganymede_icon.png";;
     esac
 
     echo "Detected new mission: $TITLE"
